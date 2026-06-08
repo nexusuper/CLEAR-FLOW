aa0b620a984c35c945d1fbf1d2962f5b38977485
Nexusuper <nexusuper@gmail.com>
2026-06-08 20:04:34 +0100

    Add CSP headers to allow Facebook SDK

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://connect.facebook.net https://www.facebook.com",
+              "style-src 'self' 'unsafe-inline'",
+              "img-src 'self' data: https: blob:",
+              "font-src 'self'",
+              "frame-src https://www.facebook.com https://web.facebook.com",
+              "connect-src 'self' https://www.facebook.com https://graph.facebook.com",
+            ].join('; '),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
